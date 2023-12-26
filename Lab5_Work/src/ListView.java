b2e16603a5f6af7a399bc047f023a4a5ecf5b288
TakaHide Ohkami <tohkami17@hotmail.com>
2023-12-25 19:50:27 -0500

    updated Lab5.

---
+++ Lab5_Work/src/ListView.java
@@ -0,0 +1,16 @@
+
+public class ListView {
+	
+	private ListViewAdapter adapter;
+	
+	public ListView() {
+	}
+	
+	public void setAdapter(ListViewAdapter adapter) {
+		this.adapter = adapter;
+	}
+
+	public void updateData() {
+		// Implement this method
+	}
+}
